2a142cfe8862b6cd2b6a78695c07dfb1443b5a07
Tanya Singh <tanyasingh788@gmail.com>
2024-02-22 11:25:53 +0530

    slicing syntax

---
+++ pattern 1.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+speed('fastest')
+pencolor('red')
+pensize(3)
+
+for i in range(8):
+    fd(80)
+    lt(45)
+    circle(100,180)
+    for i in range(6):
+        fd(50)
+        lt(60)
+
+hideturtle()
+mainloop()
+
+ 
